78fa317d40ad16c96a5df7ea915d091cc97d3044
Matthew Wells <mattwells9@shaw.ca>
2025-07-15 13:12:50 -0500

    ran Black formatter

--- bin/fastq_scan.py
+++ bin/fastq_scan.py
@@ -3,7 +3,6 @@
 """Generate summary FastQ stats in a way that fits neatly into Nextflow
 e.g. no C or external dependencies :(
 
-I realized I was being algorithmically dense when I wrote the first version of this script and that it can be refactored to use significantly less memory when running.
 
 2025-07-08: Matthew Wells
 """
@@ -21,10 +20,7 @@ import sys
 import os
 
 
-
-
-
-GZIP_MAGIC_NUMBERS = b'\x1f\x8b'
+GZIP_MAGIC_NUMBERS = b"\x1f\x8b"
 FQ_EXTENSIONSj = frozenset([".FASTQ", ".FQ"])
 
 GZIP_READER = partial(gzip.open, mode="rt")
@@ -35,44 +31,46 @@ def get_file_reader(file: p.Path) -> io.StringIO:
     """
     Return a text stream based on a files encoding. File is assumed to be plaintext
     unless there is information showing that it is compressed. As the FastQ format
-    will be validated downstream as it is not easy determining if a file is 
+    will be validated downstream as it is not easy determining if a file is
     plaintext or binary.
     """
 
-    f_reader = STD_READER 
-    with open(file, 'rb') as test_f:
+    f_reader = STD_READER
+    with open(file, "rb") as test_f:
         test_bytes = test_f.read(2)
         if test_bytes == GZIP_MAGIC_NUMBERS:
             f_reader = GZIP_READER
     return f_reader(file)
 
 
-
 class FastQData:
     """
-    This object is used for tracking FastQ data from a file as 
-    it is read. Counters are stored of quality data in the form of a 
+    This object is used for tracking FastQ data from a file as
+    it is read. Counters are stored of quality data in the form of a
     bit vector as the number of times an observation is seen can be tracked.
 
-    Sequence lenghts will be tracked as a dictionary with counts denoting a given 
-    read size, this will prevent the need of using an exceedingly large list from 
+    Sequence lenghts will be tracked as a dictionary with counts denoting a given
+    read size, this will prevent the need of using an exceedingly large list from
     being kept. In reality this could be kept in a BST
     """
-    __phred_values = 93 # Number of phred values possible
+
+    __phred_values = 93  # Number of phred values possible
     __phred_offset = 33
     _round = lambda self, x: round(x, 2)
 
-    def __init__(self, name: str, numeric_conversion: t.Callable[[int], float|decimal.Decimal]):
+    def __init__(
+        self, name: str, numeric_conversion: t.Callable[[int], float | decimal.Decimal]
+    ):
         self.name = name
         self.total_reads: int = 0
         self.total_bp: int = 0
         self.qual_sum: int = 0
         self.qual_max: int = 0
         self.qual_min: int = 0
-        self.qual_mean: None|float|decimal.Decimal = None
-        self.qual_std: None|float|decimal.Decimal = None
-        self.read_qual_mean: None|float|decimal.Decimal = None
-        self.read_qual_std: None|float|decimal.Decimal = None
+        self.qual_mean: None | float | decimal.Decimal = None
+        self.qual_std: None | float | decimal.Decimal = None
+        self.read_qual_mean: None | float | decimal.Decimal = None
+        self.read_qual_std: None | float | decimal.Decimal = None
         self.numeric_conversion = numeric_conversion
         self.quality: list[int] = [0] * self.__phred_values
         self.sequence_lengths: defaultdict[int, int] = defaultdict(lambda: 0)
@@ -82,21 +80,25 @@ class FastQData:
         self.max_sequence_length = None
         self.std_sequence_length = None
 
-    def __add__(self, obj: 'FastQData') -> 'FastQData':
+    def __add__(self, obj: "FastQData") -> "FastQData":
         """
         I dont know if it is wise to overload the __add__ method
         for combining the objects. But it seems logical to place
         the logic for object combining here.
         """
         new_data = FastQData("combined", self.numeric_conversion)
-        
+
         new_quality: list[int] = [0] * self.__phred_values
         for idx in range(self.__phred_values):
             new_quality[idx] = self.quality[idx] + obj.quality[idx]
-        
-        sequence_lengths = self._combine_dictionaries(self.sequence_lengths, obj.sequence_lengths)
-        avg_qual_read = self._combine_dictionaries(self.avg_qual_read, obj.avg_qual_read)
-        
+
+        sequence_lengths = self._combine_dictionaries(
+            self.sequence_lengths, obj.sequence_lengths
+        )
+        avg_qual_read = self._combine_dictionaries(
+            self.avg_qual_read, obj.avg_qual_read
+        )
+
         new_data.quality = new_quality
         new_data.sequence_lengths = sequence_lengths
         new_data.avg_qual_read = avg_qual_read
@@ -104,7 +106,7 @@ class FastQData:
         new_data.total_bp = self.total_bp + obj.total_bp
         new_data.qual_sum = self.qual_sum + obj.qual_sum
         return new_data
-    
+
     @staticmethod
     def _combine_dictionaries(d1, d2):
         """
@@ -128,25 +130,26 @@ class FastQData:
         self.__set_quality_metrics()
         self.__set_per_read_quality_metrics()
 
-    def to_dict(self) -> dict[str, int|float|decimal.Decimal]:
+    def to_dict(self) -> dict[str, int | float | decimal.Decimal]:
         """
-        Produce a dictionary of the final metrics to be reported. 
+        Produce a dictionary of the final metrics to be reported.
         """
         self._set_statistics()
-        data = {"total_bp": self.total_bp,
-                "total_reads": self.total_reads,
-                "qual_min": self.qual_min,
-                "qual_max": self.qual_max,
-                "qual_sum": self.qual_sum,
-                "qual_mean": self._round(self.qual_mean),
-                "qual_std": self._round(self.qual_std),
-                "read_qual_mean": self._round(self.read_qual_mean),
-                "read_qual_std": self._round(self.read_qual_std),
-                "mean_sequence_length": self._round(self.mean_sequence_length),
-                "min_sequence_length": self.min_sequence_length,
-                "max_sequence_length": self.max_sequence_length,
-                "std_sequence_length": self._round(self.std_sequence_length),  
-                }
+        data = {
+            "total_bp": self.total_bp,
+            "total_reads": self.total_reads,
+            "qual_min": self.qual_min,
+            "qual_max": self.qual_max,
+            "qual_sum": self.qual_sum,
+            "qual_mean": self._round(self.qual_mean),
+            "qual_std": self._round(self.qual_std),
+            "read_qual_mean": self._round(self.read_qual_mean),
+            "read_qual_std": self._round(self.read_qual_std),
+            "mean_sequence_length": self._round(self.mean_sequence_length),
+            "min_sequence_length": self.min_sequence_length,
+            "max_sequence_length": self.max_sequence_length,
+            "std_sequence_length": self._round(self.std_sequence_length),
+        }
         return data
 
     def __set_quality_metrics(self):
@@ -157,7 +160,6 @@ class FastQData:
         self.qual_min, self.qual_max = self.__set_quality_min_max()
         self.qual_mean = self.__set_qual_mean()
         self.qual_std = self.__set_qual_std()
-        
 
     def __set_read_length_metrics(self):
         """
@@ -170,7 +172,6 @@ class FastQData:
         self.max_sequence_length = max_
         self.std_sequence_length = std
 
-    
     def __set_per_read_quality_metrics(self):
         """
         Set the metrics require for quality per a read
@@ -179,7 +180,7 @@ class FastQData:
         std = self._dict_std(self.avg_qual_read, mean, self.total_reads)
         self.read_qual_mean = mean
         self.read_qual_std = std
-        
+
     def __set_quality_min_max(self):
         """
         calculates Phread quality min and max.
@@ -191,15 +192,17 @@ class FastQData:
             if min_idx == None and v != 0:
                 min_idx = k
             if v != 0:
-                max_idx = k # increment until the maximum quality value is found
+                max_idx = k  # increment until the maximum quality value is found
         return min_idx, max_idx
 
-    def _dict_mean(self, vector: defaultdict[int, int], total: int) -> float | decimal.Decimal:
+    def _dict_mean(
+        self, vector: defaultdict[int, int], total: int
+    ) -> float | decimal.Decimal:
         """
         Calculate the mean of a dictionary containing sequence information
         """
-        sum_ = 0 
-        min_length = float('inf')
+        sum_ = 0
+        min_length = float("inf")
         max_length = 0
         for key, value in vector.items():
             sum_ += key * value
@@ -207,18 +210,26 @@ class FastQData:
                 max_length = key
             if key < min_length:
                 min_length = key
-        return self.numeric_conversion(sum_) / self.numeric_conversion(total), min_length, max_length
-    
-    def _dict_std(self, vector: defaultdict[int, int], mean: float | decimal.Decimal, total: int):
+        return (
+            self.numeric_conversion(sum_) / self.numeric_conversion(total),
+            min_length,
+            max_length,
+        )
+
+    def _dict_std(
+        self, vector: defaultdict[int, int], mean: float | decimal.Decimal, total: int
+    ):
         """
         calculate mean of a dictionary containing sequence information
         """
-        sum_of_squares = 0 
+        sum_of_squares = 0
         for key, value in vector.items():
             diff = key - mean
             diff_sq = math.pow(diff, 2)
-            sum_of_squares += (diff_sq * value)
-        normalized_squares = self.numeric_conversion(sum_of_squares) / self.numeric_conversion(total)
+            sum_of_squares += diff_sq * value
+        normalized_squares = self.numeric_conversion(
+            sum_of_squares
+        ) / self.numeric_conversion(total)
         standard_deviation = math.sqrt(normalized_squares)
         return standard_deviation
 
@@ -226,18 +237,20 @@ class FastQData:
         """
         Calculate the standard deviation for the quality data
 
-        This function could probably be merged with '_dict_std' but it is 
+        This function could probably be merged with '_dict_std' but it is
         likely clearer having the to functions seperate as there is no need
         to pass in an iterator
         """
         qual_mean = self.qual_mean
-        sum_of_squares = 0 
+        sum_of_squares = 0
         for key, value in enumerate(self.quality):
             if value != 0:
-               diff = key - qual_mean
-               diff_sq = math.pow(diff, 2)
-               sum_of_squares += diff_sq * value
-        normalized_squares = self.numeric_conversion(sum_of_squares) / self.numeric_conversion(self.total_bp)
+                diff = key - qual_mean
+                diff_sq = math.pow(diff, 2)
+                sum_of_squares += diff_sq * value
+        normalized_squares = self.numeric_conversion(
+            sum_of_squares
+        ) / self.numeric_conversion(self.total_bp)
         standard_deviation = math.sqrt(normalized_squares)
         return standard_deviation
 
@@ -246,8 +259,9 @@ class FastQData:
         From a tuple of values calculate the mean, where the key is value and
         the second value is the frequency of it
         """
-        return self.numeric_conversion(self.qual_sum) / self.numeric_conversion(self.total_bp)
-
+        return self.numeric_conversion(self.qual_sum) / self.numeric_conversion(
+            self.total_bp
+        )
 
     def _update_quality_vector(self, quality: str):
         """
@@ -263,10 +277,12 @@ class FastQData:
             idx = ord(x) - self.__phred_offset
             qual_sum += idx
             self.quality[idx] += 1
-        read_average_quality = self.numeric_conversion(qual_sum) / self.numeric_conversion(len(quality))
+        read_average_quality = self.numeric_conversion(
+            qual_sum
+        ) / self.numeric_conversion(len(quality))
         self.avg_qual_read[read_average_quality] += 1
         self.qual_sum += qual_sum
-    
+
     def add_record(self, sequence: str, quality: str):
         """
         Add a record to the FastQ reader
@@ -287,12 +303,15 @@ def str2bool(val):
     else:
         raise argparse.ArgumentTypeError(f"Could not discern truth value of {val}")
 
+
 def verify_fastq(file, header, sequence, plus, quality):
     if not header.startswith("@"):
         sys.stderr.write(f"Fastq Header is incorrect in: {file}\n")
         sys.exit(1)
     if not (sequence and quality) or (len(sequence) != len(quality)):
-        sys.stderr.write(f"Fastq sequence and quality information incorrect in : {file}\n")
+        sys.stderr.write(
+            f"Fastq sequence and quality information incorrect in : {file}\n"
+        )
         sys.exit(1)
     if not plus:
         sys.stderr.write(f"Mangled fastq entry, missing '+' in {file}\n")
@@ -305,18 +324,32 @@ def args_in():
         description="Tabulate raw read stats of file",
     )
 
-    parser.add_argument("-f", "--files", nargs="+", help="Specify all files to be processes", required=True)
     parser.add_argument(
-        "-n", "--names", help="List of alternate names to provide the files, must match order of inputs", nargs="+"
+        "-f",
+        "--files",
+        nargs="+",
+        help="Specify all files to be processes",
+        required=True,
     )
-    parser.add_argument("-p", "--high-precision", help="Enable high precision arithmetic", default=False, type=str2bool)
+    parser.add_argument(
+        "-n",
+        "--names",
+        help="List of alternate names to provide the files, must match order of inputs",
+        nargs="+",
+    )
+    parser.add_argument(
+        "-p",
+        "--high-precision",
+        help="Enable high precision arithmetic",
+        default=False,
+        type=str2bool,
+    )
     args_out = parser.parse_args()
     return args_out
-    
+
 
 if __name__ == "__main__":
     args = args_in()
-
 
     # Verify some files were passed in
     if not len(args.files):
@@ -333,7 +366,7 @@ if __name__ == "__main__":
     if args.high_precision:
         numeric_conversion = decimal.Decimal
 
-    fq_data: dict[str, float|decimal.Decimal] = {}
+    fq_data: dict[str, float | decimal.Decimal] = {}
     combined_data = FastQData("combined", float)
     for file, name in zip(args.files, names):
         file_p = p.Path(file)
@@ -354,11 +387,5 @@ if __name__ == "__main__":
         combined_data += data
         sys.stderr.write(f"Finished reading file: {name}\n")
 
-    fq_data[combined_data.name] = combined_data.to_dict() 
+    fq_data[combined_data.name] = combined_data.to_dict()
     print(json.dumps(fq_data, indent=2))
-
-
-
-
-
-    
