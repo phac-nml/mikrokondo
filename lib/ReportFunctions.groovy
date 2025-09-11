e67cbdd105944406fbd640e5d3903e5c686b08dd
Matthew Wells <mattheww9514@gmailcom>
2025-09-11 09:52:27 -0500

    made changes to reflect PR review comments

--- lib/ReportFunctions.groovy
+++ lib/ReportFunctions.groovy
@@ -1,31 +1,17 @@
 
+class ReportFunctions {
 
+    enum FuncType {
 
-
-class ReportFunctions{
-   
-/*
-    enum FuncType{
-        GENERIC("GENERIC"),
-        CONTIG("CONTIG"),
-        READQUALITY("READQUALITY"),
-        AUTOFAIL("AUTOFAIL"),
-        COVERAGE("COVERAGE")
-         
-    }
-*/
-
-
-    enum FuncType{
         GENERIC,
         CONTIG,
         READQUALITY,
         AUTOFAIL,
-        COVERAGE 
+        COVERAGE
+
     }
 
-
-    static def select_qc_func(java.util.LinkedHashMap qual_data, java.lang.String metric, java.util.ArrayList qc_message, java.util.LinkedHashMap meta_info, java.lang.String func){
+    static def select_qc_func(java.util.LinkedHashMap qual_data, java.lang.String metric, java.util.ArrayList qc_message, java.util.LinkedHashMap meta_info, java.lang.String func) {
         def check_failed = 0
         def reisolate = 0
         def resequence = 0
@@ -34,57 +20,58 @@ class ReportFunctions{
         def checks = 0
         def function = func as FuncType
 
-        switch(function){
+        switch (function) {
             case FuncType.GENERIC:
                 (checks,
-                reisolate, 
-                resequence, 
-                failed_p, 
-                check_failed, 
-                check_ignored) = this.generic_qc_func(qual_data, metric, qc_message)
-                break;
+                reisolate,
+                resequence,
+                failed_p,
+                check_failed,
+                check_ignored) = ReportFunctions.generic_qc_func(qual_data, metric, qc_message)
+                break
             case FuncType.AUTOFAIL:
                 (checks,
-                reisolate, 
-                resequence, 
-                failed_p, 
-                check_failed, 
-                check_ignored) = this.generic_qc_func(qual_data, metric, qc_message)
-                break;
+                reisolate,
+                resequence,
+                failed_p,
+                check_failed,
+                check_ignored) = ReportFunctions.autofail_reisolate(qual_data, metric, qc_message)
+                break
             case FuncType.READQUALITY:
-                if(!meta_info.assembly){ 
+                if (!meta_info.assembly) {
                     (checks,
-                    reisolate, 
-                    resequence, 
-                    failed_p, 
-                    check_failed, 
-                    check_ignored) = this.generic_qc_func(qual_data, metric, qc_message)
+                    reisolate,
+                    resequence,
+                    failed_p,
+                    check_failed,
+                    check_ignored) = ReportFunctions.generic_qc_func(qual_data, metric, qc_message)
                 }
-                break;
+                break
             case FuncType.COVERAGE:
-                if(!meta_info.assembly){ 
+                if (!meta_info.assembly) {
                     (checks,
-                    reisolate, 
-                    resequence, 
-                    failed_p, 
-                    check_failed, 
-                    check_ignored) = this.generic_qc_func(qual_data, metric, qc_message)
-                    if(!failed_p && meta_info.downsampled){
-                        qc_message.add("The sample may have been downsampled too aggressively, if this is the cause please re-run sample with a different target depth.")
+                    reisolate,
+                    resequence,
+                    failed_p,
+                    check_failed,
+                    check_ignored) = ReportFunctions.generic_qc_func(qual_data, metric, qc_message)
+                    if (!failed_p && meta_info.downsampled) {
+                        qc_message.add('The sample may have been downsampled too aggressively, if this is the cause please re-run sample with a different target depth.')
                     }
                 }
-                break;
+                break
             case FuncType.CONTIG:
                 (checks,
-                reisolate, 
-                resequence, 
-                failed_p, 
-                check_failed, 
-                check_ignored) = this.contig_qc_func(qual_data, metric, qc_message)
-                break;
+                reisolate,
+                resequence,
+                failed_p,
+                check_failed,
+                check_ignored) = ReportFunctions.contig_qc_func(qual_data, metric, qc_message)
+                break
+            default:
+                throw NoSuchMethodExeption("No function for $func exists.")
         }
 
-        
         return [checks, reisolate, resequence, failed_p, check_failed, check_ignored]
     }
 
@@ -96,65 +83,58 @@ class ReportFunctions{
         def failed_p = false
         def checks = 0
 
-        if(qual_data && qual_data.containsKey(metric) && !qual_data[metric].status){
+        if (qual_data && qual_data.containsKey(metric) && !qual_data[metric].status) {
             checks_failed = 1
             failed_p = true
-        }else if (qual_data && (!qual_data.containsKey(metric) || !qual_data[metric].status)){
+        }else if (qual_data && (!qual_data.containsKey(metric) || !qual_data[metric].status)) {
             checks_ignored = 1
-        }else if(qual_data == null){
+        }else if (qual_data == null) {
             checks_ignored = 1
         }
         checks += 1
         return [checks, reisolate, resequence, failed_p, checks_failed, checks_ignored]
     }
-    
+
     static def generic_qc_func(java.util.LinkedHashMap qual_data, java.lang.String metric, java.util.ArrayList qc_message)     {
-
         def reisolate = 0
         def resequence = 0
         def failed_p = false
         def checks_failed = 0
         def checks_ignored = 0
         def checks = 0
-        if(qual_data && qual_data.containsKey(metric) && !qual_data[metric].status){
+        if (qual_data && qual_data.containsKey(metric) && !qual_data[metric].status) {
             reisolate = 1
             resequence = 1
             failed_p = true
             checks_failed = 1
-            //return [checks, reisolate, resequence, failed_p, checks_failed, checks_ignored] 
-        }else if(qual_data && (!qual_data.containsKey(metric) || !qual_data[metric].status)){
+        }else if (qual_data && (!qual_data.containsKey(metric) || !qual_data[metric].status)) {
             checks_ignored = 1
-
-        }else if(qual_data== null){
+        }else if (qual_data == null) {
             checks_ignored = 1
         }
         checks += 1
         return [checks, reisolate, resequence, failed_p, checks_failed, checks_ignored]
     }
 
-    static def auto_fail_reisolate(java.util.LinkedHashMap qual_data, java.lang.String metric, java.util.ArrayList qc_message)     {
-
+    static def autofail_reisolate(java.util.LinkedHashMap qual_data, java.lang.String metric, java.util.ArrayList qc_message)     {
         def reisolate = 0
         def resequence = 0
         def failed_p = false
         def checks_failed = 0
         def checks_ignored = 0
         def checks = 0
-        if(qual_data && qual_data.containsKey(metric) && !qual_data[metric].status){
+        if (qual_data && qual_data.containsKey(metric) && !qual_data[metric].status) {
             reisolate = 1
             resequence = 1
             failed_p = true
             checks_failed = 1
-            //return [checks, reisolate, resequence, failed_p, checks_failed, checks_ignored] 
-        }else if(qual_data && (!qual_data.containsKey(metric) || !qual_data[metric].status)){
+        }else if (qual_data && (!qual_data.containsKey(metric) || !qual_data[metric].status)) {
             checks_ignored = 1
-
-        }else if(qual_data== null){
+        }else if (qual_data == null) {
             checks_ignored = 1
         }
         checks += 1
         return [checks, reisolate, resequence, failed_p, checks_failed, checks_ignored]
     }
-
 
 }
