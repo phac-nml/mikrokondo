22ba4e82ba8e2f85d6ba7b5fb3a0a912a4e0eab8
Matthew Wells <mattheww9514@gmail.com>
2025-12-31 09:28:03 -0600

    ran pre-commit

--- lib/ReportFunctions.groovy
+++ lib/ReportFunctions.groovy
@@ -38,9 +38,9 @@ class ReportFunctions {
                 check_ignored) = ReportFunctions.autofail_reisolate(qual_data, metric, qc_message)
                 break
             case FuncType.READQUALITY:
-                // meta_info.assembly is always false unless only an assembly is passed to 
-                // the pipeline. If meta_info.assembly is true the function will break and 
-                // simply return the default values and will not affect the final pass fail status 
+                // meta_info.assembly is always false unless only an assembly is passed to
+                // the pipeline. If meta_info.assembly is true the function will break and
+                // simply return the default values and will not affect the final pass fail status
                 // of the pipeline.
                 if (!meta_info.assembly) {
                     (checks,
@@ -52,9 +52,9 @@ class ReportFunctions {
                 }
                 break
             case FuncType.COVERAGE:
-                // meta_info.assembly is always false unless only an assembly is passed to 
-                // the pipeline. If meta_info.assembly is true the function will break and 
-                // simply return the default values and will not affect the final pass fail status 
+                // meta_info.assembly is always false unless only an assembly is passed to
+                // the pipeline. If meta_info.assembly is true the function will break and
+                // simply return the default values and will not affect the final pass fail status
                 // of the pipeline.
                 if (!meta_info.assembly) {
                     (checks,
